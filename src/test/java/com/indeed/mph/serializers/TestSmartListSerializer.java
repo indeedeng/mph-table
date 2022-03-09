6998ab5b8371a90c0625c9a664fee386013ffbd0
Jesse Coultas <jesse@jessecoultas.com>
2022-03-08 21:24:28 -0600

    generator updates

--- src/test/java/com/indeed/mph/serializers/TestSmartListSerializer.java
+++ src/test/java/com/indeed/mph/serializers/TestSmartListSerializer.java
@@ -1,9 +1,9 @@
 package com.indeed.mph.serializers;
-
+import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
-
+import com.indeed.mph.generators.IntListGenerator;
 import java.io.IOException;
 import java.util.List;
 
@@ -26,4 +26,20 @@ public class TestSmartListSerializer {
         final SmartListSerializer<String> stringsSerializer = new SmartListSerializer<>(new SmartStringSerializer());
         assertRoundTrip(stringsSerializer, stringTarget);
     }
+    @Property
+    public void canRoundTripSerializableListsWithGenerator(
+            @From(IntListGenerator.class) final List<Integer> intTarget,
+            final List<Byte> byteTarget,
+            final List<String> stringTarget
+    ) throws IOException {
+        final SmartListSerializer<Integer> intsSerializer = new SmartListSerializer<>(new SmartIntegerSerializer());
+        assertRoundTrip(intsSerializer, intTarget);
+
+        final SmartListSerializer<Byte> bytesSerializer = new SmartListSerializer<>(new SmartByteSerializer());
+        assertRoundTrip(bytesSerializer, byteTarget);
+
+        final SmartListSerializer<String> stringsSerializer = new SmartListSerializer<>(new SmartStringSerializer());
+        assertRoundTrip(stringsSerializer, stringTarget);
+    }
 }
+
